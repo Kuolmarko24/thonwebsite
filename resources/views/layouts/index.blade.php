00fca9a158b6e4406496205e2f6a6a6d9b1f8004
Kuolmarko24 <kuolmarko47@gmail.com>
2022-03-02 15:40:12 +0300

    database changed to pgsql

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -105,7 +105,7 @@
                         <div class="footer__social__links">
                             <a href="https://facebook.com/DENGAJÖK PICTURES"><i class="fa fa-facebook"></i></a>
                             {{-- <a href="#"><i class="fa fa-twitter"></i></a> --}}
-                            <a href="https://instagram.com/kuolmarko"><i class="fa fa-instagram"></i></a>
+                            <a href="#"><i class="fa fa-instagram"></i></a>
             
                         </div>
                     </div>
